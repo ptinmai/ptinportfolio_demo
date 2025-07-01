da7986aa294d707dd50169b9a8f74a515e02fff2
ptinmai <tien.mtp669@arena-multimedia.edu.vn>
2025-07-02 00:19:12 +0700

    Add files via upload

---
+++ JS/control.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+strings: ["Web Designer.", "Code Developer.","Apps Designer.","Film Maker."],
+typeSpeed:20,
+backSpeed:20,
+loop:true,
+});
+
+$('.timeline-panel').click(function() {
+    $('.timeline-body', this).toggle(); // p00f
+});
+
+$('.counter').countUp();
+
+AOS.init();
